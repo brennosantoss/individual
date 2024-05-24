93f662b823705fb11fda29ef159e13185f0e13fc
brennosantoss <brenno.alves@sptech.school>
2024-05-24 00:53:06 -0300

    pagina league of legends

---
+++ public/scripts/lolapi.js
@@ -0,0 +1,35 @@
+
+const API_URL = 'https://ddragon.leagueoflegends.com/cdn/12.9.1/data/pt_BR/champion.json';
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchChampions();
+});
+
+async function fetchChampions() {
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        displayChampions(data.data);
+    } catch (error) {
+        console.error('Erro ao buscar dados dos campeões:', error);
+    }
+}
+
+function displayChampions(champions) {
+    const championsContainer = document.getElementById('champions');
+    Object.keys(champions).forEach(key => {
+        const champion = champions[key];
+        const championElement = document.createElement('div');
+        championElement.className = 'champion';
+
+        const tags = champion.tags.join(', ');
+
+        championElement.innerHTML = `
+            <img src="http://ddragon.leagueoflegends.com/cdn/12.9.1/img/champion/${champion.image.full}" alt="${champion.name}">
+            <h2>${champion.name}</h2>
+            <p>${champion.title}</p>
+            <p><strong>Classes:</strong> ${tags}</p>
+        `;
+        championsContainer.appendChild(championElement);
+    });
+}
