f25855d1cd0f7a6ce2c7a2a94d77ddf9c6249fb6
brennosantoss <brenno.alves@sptech.school>
2024-06-02 23:56:45 -0300

    dashbord

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,14 +1,7 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database individual;
 
-/*
-comandos para mysql server
-*/
+use individual;
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
@@ -18,32 +11,33 @@ CREATE TABLE usuario (
     dtcadastro datetime
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
+insert into usuario (nome,email,senha,dtcadastro) values
+('robson','robson@gmail','123',now());
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
-);
+create table jogo(
+idjogo int primary key auto_increment,
+nome varchar(20),
+tipo varchar(20));
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+insert into jogo (nome,tipo) values 
+('League of Legends','MOBA'),
+('Counter Strike','FPS'),
+('Valorant','FPS');
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
 
-insert into aquario (descricao) values ('Aquário de Estrela-do-mar');
+
+create table jogo_interesse(
+idinteresse int auto_increment,
+fkusuario int,
+fkjogo int,
+nivel int,
+primary key(idinteresse,fkusuario,fkjogo),
+foreign key(fkusuario) references usuario (id),
+foreign key (fkjogo) references jogo (idjogo));
+
+
+select * from usuario;
+
+select * from jogo;
+
+select * from jogo_interesse;
--- src/models/jogo_interesseModel.js
+++ src/models/jogo_interesseModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config");
 
-function inserirJogos(id,fkjogo) {
-  var instrucaoSql = `insert into jogo_interesse (fkusuario,fkjogo,nivel)= '${id}' , ${fkjogo}, ${nivel}`;
+function inserirJogos(idjogo,fkjogo) {
+  var instrucaoSql = `insert into jogo_interesse (fkusuario,fkjogo,nivel)= '${idjogo}' , ${fkjogo}, ${nivel}`;
 
   return database.executar(instrucaoSql);
 }
